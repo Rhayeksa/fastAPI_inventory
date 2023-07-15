77c8cdeb4cd56f01b35675ef05d2c476eaf4d42b
rhayeksa <rhayeksads@gmail.com>
2023-07-16 05:18:17 +0700

    updated

--- src/config/project.py
+++ src/config/project.py
@@ -1,12 +1,12 @@
-from os import environ
+# from os import environ
 
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
 
-env = dotenv_values(".env")
+# env = dotenv_values(".env")
 
-PROJECT = environ.get("PROJECT") if environ.get(
-    "PROJECT") != None else env["PROJECT"]
-VERSION = environ.get("VERSION") if environ.get(
-    "VERSION") != None else env["VERSION"]
-DESCRIPTION = environ.get("DESCRIPTION") if environ.get(
-    "DESCRIPTION") != None else env["DESCRIPTION"]
+# PROJECT = environ.get("PROJECT") if environ.get(
+#     "PROJECT") != None else env["PROJECT"]
+# VERSION = environ.get("VERSION") if environ.get(
+#     "VERSION") != None else env["VERSION"]
+# DESCRIPTION = environ.get("DESCRIPTION") if environ.get(
+#     "DESCRIPTION") != None else env["DESCRIPTION"]
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from src.routes import routes
-from src.config.project import PROJECT, VERSION, DESCRIPTION
+# from src.config.project import PROJECT, VERSION, DESCRIPTION
 
 app = FastAPI(
-    title=PROJECT or "FastAPI",
-    description=DESCRIPTION or "FastAPI",
-    version=VERSION or "0.1.0",
+    # title=PROJECT or "FastAPI",
+    # description=DESCRIPTION or "FastAPI",
+    # version=VERSION or "0.1.0",
 )
 
 for route in routes:
