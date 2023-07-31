a7b43776fdf44c4c74e23eb4216114c5cedf1310
rhayeksa <rhayeksads@gmail.com>
2023-08-01 06:35:12 +0700

    implement origin corsmiddleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.config.db.inventory import engine
 from src.config.project import DESCRIPTION, PROJECT, VERSION
@@ -7,10 +8,21 @@ from src.routes import routes
 
 metadata.create_all(bind=engine)
 
+
 app = FastAPI(
     title=PROJECT or "FastAPI",
     description=DESCRIPTION or "FastAPI",
     version=VERSION or "0.1.0",
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 for route in routes:
